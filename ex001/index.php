5250d5c108de04270b0c0118bffc0fad51daa3be
Luiz Araújo <araujofh58@gmail.com>
2023-03-02 14:19:24 -0300

    Chamando Data e Hora

--- ex001/index.php
+++ ex001/index.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dados do Servidor</title>
+    <title>PHP</title>
 </head>
 <body>
-    <h2>Dados do Servidor</h2>
+    <h2>Exemplo do Php</h2>
     <?php 
-        phpinfo();
+        date_default_timezone_set("America/Sao_Paulo");
+        echo "Hoje é dia: " . date("d/m/y"); 
+        echo " e a hora é: ". date("g:i:s");
     
     ?>
 </body>
