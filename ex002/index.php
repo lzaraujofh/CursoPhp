f66d7ac5ca95263dd7f86972ef667b2176727585
Luiz Araújo <araujofh58@gmail.com>
2023-03-02 15:41:15 -0300

    Create index.php

---
+++ ex002/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h2> Trabalhando com PhP </h2>
+    <?php 
+    $nome="Luiz Araújo";
+    echo "O seu nome é: " . $nome;
+      
+    $salario=2500.75;
+    echo " O seu salário é: $salario";
+    
+    
+    ?>
+</body>
+</html>
