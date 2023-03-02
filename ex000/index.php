d5e356817a971a30f54121016373d8b944869751
Luiz Araújo <araujofh58@gmail.com>
2023-03-01 22:42:36 -0300

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Php</title>
+</head>
+<body>
+    <h1>
+        <?php 
+          $nome = "Beatles";
+          echo "A maior banda do mundo: $nome!";
+        ?>
+    </h1>
+    <h2>
+        <? 
+        
+        ?>
+
+    </h2>
+
+</body>
+</html>
