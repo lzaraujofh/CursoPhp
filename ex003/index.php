83728ddf74514f1ac691439982e9a6944bebaab9
Luiz Araújo <araujofh58@gmail.com>
2023-03-05 01:15:25 -0300

    Formulários

---
+++ ex003/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em Php</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 1.3em;
+        }
+
+    </style>
+</head>
+<body>
+    <h2>Teste de Tipos Primitivos</h2>
+    <?php 
+    $num="Luiz Araújo";
+    echo var_dump($num);
+       
+    $nom = "Rodrigo";
+    $snom = "Nogueira";
+    echo $nom." Minotauro ".$snom;
+    
+    
+    ?>
+</body>
+</html>
